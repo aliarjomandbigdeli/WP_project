18cf94e29b72f402a520fbbf79b12d9a11708735
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-23 12:27:59 +0430

    add MyHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,6 @@
 <template>
     <div id="app">
-        <div id="nav-bar-div">
-            <div class="nav-bar-items"><a href="login.html#login_sec">ورود</a></div>
-            <div class="nav-bar-items" id="separator">|</div>
-            <div class="nav-bar-items"><a href="login.html#register_sec">عضویت</a></div>
-            <div class="nav-bar-items">&nbsp;&nbsp;&nbsp;</div>
-            <div class="nav-bar-items"><a href="">راهنما</a></div>
-        </div>
-        <div id="nav-border-div"></div>
+        <MyHeader></MyHeader>
 
 
     </div>
@@ -20,10 +13,12 @@
 
 <script>
     // import HelloWorld from './components/HelloWorld.vue'
+    import MyHeader from './components/MyHeader.vue'
 
     export default {
         name: 'app',
         components: {
+            MyHeader
             // HelloWorld
         }
     }
@@ -36,50 +31,5 @@
             display: none;
         }
     }
-
-    #nav-bar-div {
-        display: flex;
-        flex-direction: row-reverse;
-        padding-left: 30px;
-        padding-right: 30px;
-        font-size: 0.9em;
-        color: #4a5575;
-    }
-
-    .nav-bar-items {
-        margin: 10px;
-        padding: 5px;
-        background-color: white;
-    }
-
-    .nav-bar-items > a {
-        color: #4a5575;
-    }
-
-    .nav-bar-items > a:hover {
-        color: var(--btn-bg-or-highlighted-color);
-    }
-
-    #separator {
-        margin-left: 0;
-        margin-right: 0;
-        padding-right: 0;
-        padding-left: 0;
-    }
-
-    #nav-border-div {
-        background: linear-gradient(to right, red, purple);
-        height: 3px;
-    }
-
-
-    /*#app {*/
-    /*    font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
-    /*    -webkit-font-smoothing: antialiased;*/
-    /*    -moz-osx-font-smoothing: grayscale;*/
-    /*    text-align: center;*/
-    /*    color: #2c3e50;*/
-    /*    margin-top: 60px;*/
-    /*}*/
 </style>
 <style src="./global.css"></style>
---
+++ src/components/MyHeader.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <div id="nav-bar-div">
+            <div class="nav-bar-items"><a href="login.html#login_sec">ورود</a></div>
+            <div class="nav-bar-items" id="separator">|</div>
+            <div class="nav-bar-items"><a href="login.html#register_sec">عضویت</a></div>
+            <div class="nav-bar-items">&nbsp;&nbsp;&nbsp;</div>
+            <div class="nav-bar-items"><a href="">راهنما</a></div>
+        </div>
+        <div id="nav-border-div"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "MyHeader"
+    }
+</script>
+
+<style scoped>
+    #nav-bar-div {
+        display: flex;
+        flex-direction: row-reverse;
+        padding-left: 30px;
+        padding-right: 30px;
+        font-size: 0.9em;
+        color: #4a5575;
+    }
+
+    .nav-bar-items {
+        margin: 10px;
+        padding: 5px;
+        background-color: white;
+    }
+
+    .nav-bar-items > a {
+        color: #4a5575;
+    }
+
+    .nav-bar-items > a:hover {
+        color: var(--btn-bg-or-highlighted-color);
+    }
+
+    #separator {
+        margin-left: 0;
+        margin-right: 0;
+        padding-right: 0;
+        padding-left: 0;
+    }
+
+    #nav-border-div {
+        background: linear-gradient(to right, red, purple);
+        height: 3px;
+    }
+</style>

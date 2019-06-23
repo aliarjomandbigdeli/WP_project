08cf7e47ab05c5960957e5a12e7f1e56bfa34b49
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-23 12:14:55 +0430

    add global.css to vue

--- src/App.vue
+++ src/App.vue
@@ -30,79 +30,6 @@
 </script>
 
 <style>
-
-    @font-face {
-        font-family: Shabnam;
-        src: url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam.eot");
-        src: url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam.eot?#iefix") format("embedded-opentype"), url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam.woff") format("woff"), url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam.woff2") format("woff2"), url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam.ttf") format("truetype");
-        font-weight: 400
-    }
-
-    @font-face {
-        font-family: Shabnam;
-        src: url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam-Bold.eot");
-        src: url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam-Bold.eot?#iefix") format("embedded-opentype"), url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam-Bold.woff") format("woff"), url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam-Bold.woff2") format("woff2"), url("https://dist.reyhoon-static.com/web-assets/fonts/v3.0.1/Shabnam-Bold.ttf") format("truetype");
-        font-weight: 700
-    }
-
-    a {
-        text-decoration: none;
-        color: black;
-    }
-
-    * {
-        font-family: Shabnam;
-    }
-
-    body {
-        margin: 0;
-        /*color name*/
-        --main-bg-color: #f6f9fc;
-        --btn-bg-or-highlighted-color: #d40062;
-        --loign-register-txt-color: gray;
-        --loign-register-bg-color: #f6f9fc;
-    }
-
-    /*login, register footer style*/
-    #footer-log-reg-div {
-        background-color: white;
-    }
-
-    #footer-log-reg-social-div {
-        display: flex;
-        justify-content: center;
-        margin: 10px;
-    }
-
-    #footer-log-reg-social-div > a {
-        background-color: gray;
-        color: white;
-        border-radius: 20px;
-        margin: 10px;
-    }
-
-    #footer-log-reg-social-div > a > i {
-        padding: 10px;
-    }
-
-    #footer-log-reg-border-div {
-        height: 2px;
-        background-color: var(--loign-register-bg-color);
-    }
-
-    #footer-log-reg-more-option-div {
-        display: flex;
-        justify-content: center;
-        color: var(--loign-register-txt-color);
-        padding: 10px;
-    }
-
-    #footer-log-reg-more-option-div > * {
-        margin: 10px;
-        color: var(--loign-register-txt-color);
-    }
-
-
     /* Responsive layout - when the screen is less than 900px wide, hide sms-img */
     @media screen and (max-width: 1050px) {
         #sms-img {
@@ -146,7 +73,6 @@
     }
 
 
-
     /*#app {*/
     /*    font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
     /*    -webkit-font-smoothing: antialiased;*/
@@ -156,3 +82,4 @@
     /*    margin-top: 60px;*/
     /*}*/
 </style>
+<style src="./global.css"></style>

46122667b4320d10f1462797cffac09a92a81cfe
aliarjomandbigdeli <aliabigdeli@gmail.com>
2019-06-24 12:58:22 +0430

    get data by ajax

--- src/components/BestOfMonth.vue
+++ src/components/BestOfMonth.vue
@@ -7,73 +7,108 @@
 
         <h2 id="best-of-the-month">رستوران‌‌ها و فست فود‌های برتر ماه بر اساس امتیاز‌دهی کاربران</h2>
         <div id="best-of-the-month-div">
-            <a href="">
-                <div class="best-month-div-items" id="best-month-item-3">
-                    <div><img src="../../mocks/restaurants/bst-rest-1.jpg"></div>
-                    <h2>(باگت (اندرزگو </h2>
+            <a href="" v-for="detail in restaurants.slice(0,3)" :key="detail.name">
+                <div class="best-month-div-items">
+                    <div><img height="82" width="82" :src="detail.imgUrl"></div>
+                    <h2>{{detail.name}}</h2>
                     <div class="stars-bar">
-                        ----- (<span class="rate-participant">81</span>)
+                        ----- (<span class="rate-participant">{{detail.numOfRates}}</span>)
                         <span class="stars">
-                <i class="far fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                </span>
-                        &nbsp;<span class="rate-number">3.8</span>&nbsp;-----
+                        <i class="far fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                        &nbsp;<span class="rate-number">{{detail.rate}}</span>&nbsp;-----
                     </div>
                     <p class="foods">فست فود &#9679; پیتزا &#9679; برگر &#9679; ساندویچ</p>
-                    <p class="address">بلوار اندرزگو، بین کاوه و قیطریه، نبش مهر محمدی جنوبی</p>
+                    <p class="address">{{detail.address}}</p>
                     <div class="order-bst-rest-btn">شروع سفارش</div>
                 </div>
             </a>
-            <a href="">
-                <div class="best-month-div-items" id="best-month-item-2">
-                    <div><img src="../../mocks/restaurants/bst-rest-2.jpg"></div>
-                    <h2>(شیلا (مطهری </h2>
-                    <div class="stars-bar">
-                        ----- (<span class="rate-participant">36</span>)
-                        <span class="stars">
-                <i class="far fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                </span>
-                        &nbsp;<span class="rate-number">3.8</span>&nbsp;-----
-                    </div>
-                    <p class="foods">فست فود &#9679; پیتزا &#9679; برگر &#9679; ساندویچ</p>
-                    <p class="address">ولیعصر، مطهری، فتحی شقاقی، زیر برج بلورین</p>
-                    <div class="order-bst-rest-btn">شروع سفارش</div>
-                </div>
-            </a>
-            <a href="">
-                <div class="best-month-div-items" id="best-month-item-1">
-                    <div><img src="../../mocks/restaurants/bst-rest-3.jpg"></div>
-                    <h2>(فست فود پیتزاهات (سعادت آباد </h2>
-                    <div class="stars-bar">
-                        ----- (<span class="rate-participant">69</span>)
-                        <span class="stars">
-                <i class="far fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                <i class="fas fa-star"></i>
-                </span>
-                        &nbsp;<span class="rate-number">3.8</span>&nbsp;-----
-                    </div>
-                    <p class="foods">فست فود &#9679; پیتزا &#9679; برگر &#9679; ساندویچ</p>
-                    <p class="address">سعادت آباد، پاساژ میلاد نور، طبقه همکف</p>
-                    <div class="order-bst-rest-btn">شروع سفارش</div>
-                </div>
-            </a>
+
+
+<!--                        <a href="">-->
+<!--                            <div class="best-month-div-items" id="best-month-item-3">-->
+<!--                                <div><img src="../../mocks/restaurants/bst-rest-1.jpg"></div>-->
+<!--                                <h2>(باگت (اندرزگو </h2>-->
+<!--                                <div class="stars-bar">-->
+<!--                                    -&#45;&#45;&#45;&#45; (<span class="rate-participant">81</span>)-->
+<!--                                    <span class="stars">-->
+<!--                            <i class="far fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            </span>-->
+<!--                                    &nbsp;<span class="rate-number">3.8</span>&nbsp;-&#45;&#45;&#45;&#45;-->
+<!--                                </div>-->
+<!--                                <p class="foods">فست فود &#9679; پیتزا &#9679; برگر &#9679; ساندویچ</p>-->
+<!--                                <p class="address">بلوار اندرزگو، بین کاوه و قیطریه، نبش مهر محمدی جنوبی</p>-->
+<!--                                <div class="order-bst-rest-btn">شروع سفارش</div>-->
+<!--                            </div>-->
+<!--                        </a>-->
+<!--                        <a href="">-->
+<!--                            <div class="best-month-div-items" id="best-month-item-2">-->
+<!--                                <div><img src="../../mocks/restaurants/bst-rest-2.jpg"></div>-->
+<!--                                <h2>(شیلا (مطهری </h2>-->
+<!--                                <div class="stars-bar">-->
+<!--                                    -&#45;&#45;&#45;&#45; (<span class="rate-participant">36</span>)-->
+<!--                                    <span class="stars">-->
+<!--                            <i class="far fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            </span>-->
+<!--                                    &nbsp;<span class="rate-number">3.8</span>&nbsp;-&#45;&#45;&#45;&#45;-->
+<!--                                </div>-->
+<!--                                <p class="foods">فست فود &#9679; پیتزا &#9679; برگر &#9679; ساندویچ</p>-->
+<!--                                <p class="address">ولیعصر، مطهری، فتحی شقاقی، زیر برج بلورین</p>-->
+<!--                                <div class="order-bst-rest-btn">شروع سفارش</div>-->
+<!--                            </div>-->
+<!--                        </a>-->
+<!--                        <a href="">-->
+<!--                            <div class="best-month-div-items" id="best-month-item-1">-->
+<!--                                <div><img src="../../mocks/restaurants/bst-rest-3.jpg"></div>-->
+<!--                                <h2>(فست فود پیتزاهات (سعادت آباد </h2>-->
+<!--                                <div class="stars-bar">-->
+<!--                                    -&#45;&#45;&#45;&#45; (<span class="rate-participant">69</span>)-->
+<!--                                    <span class="stars">-->
+<!--                            <i class="far fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            <i class="fas fa-star"></i>-->
+<!--                            </span>-->
+<!--                                    &nbsp;<span class="rate-number">3.8</span>&nbsp;-&#45;&#45;&#45;&#45;-->
+<!--                                </div>-->
+<!--                                <p class="foods">فست فود &#9679; پیتزا &#9679; برگر &#9679; ساندویچ</p>-->
+<!--                                <p class="address">سعادت آباد، پاساژ میلاد نور، طبقه همکف</p>-->
+<!--                                <div class="order-bst-rest-btn">شروع سفارش</div>-->
+<!--                            </div>-->
+<!--                        </a>-->
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "BestOfMonth"
+        name: "BestOfMonth",
+
+        data() {
+            return {
+                restaurants: []
+            }
+        },
+        mounted() {
+            fetch("http://demo2469824.mockable.io/best-restaurants")
+                .then(response => response.json())
+                .then((data) => {
+                    this.restaurants = data.restaurants;
+                })
+        }
     }
 </script>
 
